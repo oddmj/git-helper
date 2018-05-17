03f683487eaa29887acaeeaa058bdd013308347f
laranhee <laranhee@gmail.com>
2018-05-17 22:10:47 +0900

    브랜치 리스트 노출 구현, material-icons 추가

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -2,6 +2,21 @@
   <v-app dark>
     <v-btn @click="selectGitProject">프로젝트 열기</v-btn>
     <p class="title text-xs-center">{{projectName}}</p>
+    <v-list>
+      <v-list-tile v-for="branch in branches" :key="branch.name">
+        <v-list-tile-action>
+          <v-checkbox v-if="branch.isDeletable" v-model="branch.isSelected"/>
+        </v-list-tile-action>
+        <v-list-tile-content>
+            <v-list-tile-title>
+              {{branch.name}}
+            </v-list-tile-title>
+            <v-list-tile-sub-title v-if="!branch.isDeletable">
+              {{ branch.nonDeletableReason }}
+            </v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
   </v-app>
 </template>
 
@@ -14,6 +29,7 @@ export default {
   data() {
     return {
       projectName: null,
+      branches: null,
     };
   },
   methods: {
@@ -22,11 +38,14 @@ export default {
         .selectGitProject()
         .then((path) => {
           this.projectName = path.split('/').pop();
-          // this.showBranches();
+          this.showBranches();
         })
         .catch(() => {
           // alert('깃 프로젝트가 아닙니다');
         });
+    },
+    showBranches() {
+      this.branches = service.getBranches();
     },
   },
 };
--- src/renderer/service/service.js
+++ src/renderer/service/service.js
@@ -30,4 +30,34 @@ function selectGitProject() {
   return showGitProjectOpenDialog().then((path) => validateGitProject(path));
 }
 
-export default { selectGitProject };
+function createBranchObject(branch) {
+  if (branch.startsWith('* ')) {
+    return {
+      name: branch.substr(2),
+      isCheckedOut: true,
+      nonDeletableReason: `체크아웃 중인 브랜치`,
+    };
+  }
+
+  const branchName = branch.substr(2);
+  if (branchName === 'develop' || branchName === 'master') {
+    return {
+      name: branchName,
+      isDeletable: false,
+      nonDeletableReason: `${branchName} 브랜치`,
+    };
+  }
+
+  return {
+    name: branchName,
+    isDeletable: true,
+  };
+}
+
+function getBranches() {
+  const result = exec('git branch').stdout.split('\n');
+  result.pop();
+  return result.map((branch) => createBranchObject(branch));
+}
+
+export default { selectGitProject, getBranches };
