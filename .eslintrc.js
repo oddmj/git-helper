d8250d9c020c7609602efa1581953c2c6e04aa7f
laranhee <laranhee@gmail.com>
2018-05-18 00:22:17 +0900

    빌드 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,32 @@ module.exports = {
     'import/extensions': 0,
     'import/newline-after-import': 0,
     'no-multi-assign': 0,
+    // TODO FIXME electron이 해당 룰에 걸려 임시로 처리. 해결방안 찾을 것
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'test/**', // tape, common npm pattern
+          'tests/**', // also common npm pattern
+          'spec/**', // mocha, rspec-like pattern
+          '**/__tests__/**', // jest pattern
+          'test.{js,jsx}', // repos with a single test file
+          'test-*.{js,jsx}', // repos with multiple top-level test files
+          '**/*.{test,spec}.{js,jsx}', // tests where the extension denotes that it is a test
+          '**/jest.config.js', // jest config
+          '**/webpack.config.js', // webpack config
+          '**/webpack.config.*.js', // webpack config
+          '**/rollup.config.js', // rollup config
+          '**/rollup.config.*.js', // rollup config
+          '**/gulpfile.js', // gulp config
+          '**/gulpfile.*.js', // gulp config
+          '**/Gruntfile{,.js}', // grunt config
+          '**/protractor.conf.*.js', // protractor config
+          '**',
+        ],
+        optionalDependencies: false,
+      },
+    ],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
   },
--- src/main/index.js
+++ src/main/index.js
@@ -5,7 +5,7 @@ import { app, BrowserWindow, ipcMain, dialog } from 'electron'; // eslint-disabl
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path')
+  global.__static = require('path') // eslint-disable-line
     .join(__dirname, '/static')
     .replace(/\\/g, '\\\\'); // eslint-disable-line
 }
