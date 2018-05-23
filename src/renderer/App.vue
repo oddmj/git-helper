68108bfa22331eb7391363562ad049dc696903b7
laranhee <laranhee@gmail.com>
2018-05-23 09:35:14 +0900

    프로젝트 열기 안내 화면 추가

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,23 +1,32 @@
 <template>
   <v-app dark>
-    <v-btn @click="selectGitProject">프로젝트 열기</v-btn>
-    <p class="title text-xs-center">{{projectName}}</p>
-    <v-btn @click="openDeleteDialog" v-if="isSelectedBranchExist">브랜치 삭제하기</v-btn>
-    <v-list>
-      <v-list-tile v-for="branch in branches" :key="branch.name">
-        <v-list-tile-action>
-          <v-checkbox v-if="branch.isDeletable" v-model="branch.isSelected"/>
-        </v-list-tile-action>
-        <v-list-tile-content>
-            <v-list-tile-title>
-              {{branch.name}}
-            </v-list-tile-title>
-            <v-list-tile-sub-title v-if="!branch.isDeletable">
-              {{ branch.nonDeletableReason }}
-            </v-list-tile-sub-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
+    <v-container>
+      <v-btn @click="selectGitProject">프로젝트 열기</v-btn>
+      <v-btn @click="openDeleteDialog" v-if="isSelectedBranchExist">브랜치 삭제하기</v-btn>
+      <template v-if="branches.length">      
+        <v-divider></v-divider>
+        <p class="title">{{projectName}}</p>
+        <v-divider></v-divider>
+        <v-list>
+          <v-list-tile v-for="branch in branches" :key="branch.name">
+            <v-list-tile-action>
+              <v-checkbox v-if="branch.isDeletable" v-model="branch.isSelected"/>
+            </v-list-tile-action>
+            <v-list-tile-content>
+                <v-list-tile-title>
+                  {{branch.name}}
+                </v-list-tile-title>
+                <v-list-tile-sub-title v-if="!branch.isDeletable">
+                  {{ branch.nonDeletableReason }}
+                </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </template>
+      <template v-else>
+        <p class="title">프로젝트를 선택해 주세요.</p>
+      </template>
+    </v-container>
     <v-dialog v-model="deleteDialog" width="290" persistent>
       <v-card>
         <v-card-title>선택한 브랜치를 삭제하시겠습니까?</v-card-title>
