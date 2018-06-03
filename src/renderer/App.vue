1b2ad11259e7080a27900630706f435477380bce
laranhee <laranhee@gmail.com>
2018-06-03 15:05:52 +0900

    좌측메뉴 추가

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app dark>
-    <v-container>
-      <v-btn @click="selectGitProject">프로젝트 열기</v-btn>
-      <v-btn @click="openDeleteDialog" v-if="isSelectedBranchExist">브랜치 삭제하기</v-btn>
+    <v-navigation-drawer app permanent>
+      <v-btn icon @click="selectGitProject">
+        <v-icon>add</v-icon>
+      </v-btn>
+      <div>{{projectName}}</div>
+    </v-navigation-drawer>
+    <v-content>
       <template v-if="branches.length">      
         <v-divider></v-divider>
         <p class="title">{{projectName}}</p>
         <v-divider></v-divider>
+        <v-btn @click="openDeleteDialog" v-if="isSelectedBranchExist">브랜치 삭제하기</v-btn>
         <v-list>
           <v-list-tile v-for="branch in branches" :key="branch.name">
             <v-list-tile-action>
@@ -26,7 +31,7 @@
       <template v-else>
         <p class="title">프로젝트를 선택해 주세요.</p>
       </template>
-    </v-container>
+    </v-content>
     <v-dialog v-model="deleteDialog" width="290" persistent>
       <v-card>
         <v-card-title>선택한 브랜치를 삭제하시겠습니까?</v-card-title>
